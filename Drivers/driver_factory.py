a8392a2039336b15959dee2fa7155eb1f17a665a
sanjayr <sanjay.r@hilabs.com>
2025-01-07 20:01:40 +0530

    linux changes

--- Drivers/driver_factory.py
+++ Drivers/driver_factory.py
@@ -13,6 +13,11 @@ class DriverFactory:
             options.add_argument("start-maximized")
             if config["headless_mode"] is True:
                 options.add_argument("--headless")
+                options.add_argument("--no-sandbox")  # Required for Linux
+                options.add_argument("--disable-dev-shm-usage")  # Overcomes limited resources in containers
+                options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration (optional for stability)
+                options.add_argument("--window-size=1920,1080")  # Set fixed size for headless
+                options.add_argument("--remote-debugging-port=9222")  # Debugging support if required
             driver_obj = webdriver.Chrome(service=Service(executable_path=binary_path), options=options)
             driver = WebDriverExtended(driver_obj, WebDriverListener(testcase_name), config)
             '''driver = WebDriverExtended(
